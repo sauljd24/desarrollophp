be55a0beaf7434531e2c86b18abfdff0079c9d6b
saul <sjdefy@gmail.com>
2025-02-23 17:13:23 -0600

    Agregando gitignore

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php phpinfo(); ?>
+<!DOCTYPE HTML>
+    <head>
+        <meta charset="utf-8" />
+        <title> Aprendiendo PHP - en Master PHP </title>
+    </head>
+    <body>
+
+        <h1> Master en PHP - Saul Jurado</h1>
+        
+        <?php 
+            echo '<h3>Listado de Juegos:</h3>'; 
+            echo '<ul>'
+            .'<li>Elder Ring</li>'
+            .'<li>Elder Scroll</li>'
+            .'<li>Dark Soul 3</li>'
+            .'</ul>';
+
+        ?>
+    </body>
+</html>
+
